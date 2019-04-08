9a52340f1e8e378bf12f506a627a8a363b09104f
Garrett Holmes <Garrett.Holmes@gmail.com>
2019-04-07 22:09:53 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// Scrape LCRA Website
+//https://hydromet.lcra.org/Reports/RiverStageFlow?fbclid=IwAR3GfSR9W0WX9lCZurJQayhX8sgA09bNvQXHmB_wpWnx4IZc20GHnKIR7eY
+
+var axios = require("axios");
+
+console.log(axios);
